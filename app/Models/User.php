f6d2ab9ec7535c97ca42e68a083e870e33b617c0
Sunrise <linkphper@gmail.com>
2018-06-04 18:32:03 +0800

    notification

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,10 +5,13 @@ namespace App\Models;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use App\Models\Reply;
+use Auth;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable {
+        notify as protected laravelNotify;
+    }
 
     /**
      * The attributes that are mass assignable.
@@ -18,6 +21,17 @@ class User extends Authenticatable
     protected $fillable = [
         'name', 'email', 'password', 'introduction', 'avatar'
     ];
+
+    public function notify($instance)
+    {
+        // 如果要通知的人是当前用户，就不必通知了！
+        if ($this->id === Auth::id()) {
+            return false;
+        }
+
+        $this->increment('notification_count');
+        $this->laravelNotify($instance);
+    }
 
     /**
      * The attributes that should be hidden for arrays.
